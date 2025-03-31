35ae1c22aa62cb94d80dfbceea1ae4a4501eb651
ecollin2 <ecollin@seas.upenn.edu>
2025-03-31 09:22:15 -0600

    Update listener.py

--- listener.py
+++ listener.py
@@ -72,7 +72,7 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         df.to_csv(eventfile, index=False)
     else:
         all_rows = []
-	for block_num in range(start_block,end_block+1):
+        for block_num in range(start_block, end_block + 1):
         	event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
             	events = event_filter.get_all_entries()
             #print( f"Got {len(events)} entries for block {block_num}" )
