b8cd6fa19e5f49bcc4178e3e75a41cc501fa64a7
ecollin2 <ecollin@seas.upenn.edu>
2025-03-31 09:09:39 -0600

    Update listener.py

--- listener.py
+++ listener.py
@@ -55,9 +55,39 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         events = event_filter.get_all_entries()
         #print( f"Got {len(events)} entries for block {block_num}" )
         # TODO YOUR CODE HERE
+	rows = []
+        for evt in events:
+            data = {
+                'chain': chain,
+                'token': evt.args['token'],
+                'recipient': evt.args['recipient'],
+                'amount': evt.args['amount'],
+                'transactionHash': evt.transactionHash.hex(),
+                'address': evt.address,
+                'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            }
+            rows.append(data)
+
+        df = pd.DataFrame(rows)
+        df.to_csv(eventfile, index=False)
     else:
+	all_rows[]
         for block_num in range(start_block,end_block+1):
             event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
             events = event_filter.get_all_entries()
             #print( f"Got {len(events)} entries for block {block_num}" )
             # TODO YOUR CODE HERE
+            for evt in events:
+                data = {
+                    'chain': chain,
+                    'token': evt.args['token'],
+                    'recipient': evt.args['recipient'],
+                    'amount': evt.args['amount'],
+                    'transactionHash': evt.transactionHash.hex(),
+                    'address': evt.address,
+                    'date': datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                }
+                all_rows.append(data)
+
+        df = pd.DataFrame(all_rows)
+        df.to_csv(eventfile, index=False)
